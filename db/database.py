9dc2dbb67136c2693cdd00eef0d23c71a47a6a77
Iván L. Murillo <diskman199@gmail.com>
2021-12-01 18:40:03 -0600

    Register users and login

--- db/database.py
+++ db/database.py
@@ -9,6 +9,41 @@ class Database:
                 password = 'AgendApp',
                 database = 'agendapp'
             )
-        except:
+            self.cursor = self.connection.cursor()
+        except Exception as e:
             print("No se pudo conectar a la base de datos")
+            print(e)
             raise
+
+    def agregar_usuario(self, correo, contraseña, nombre):
+        if self.existe_usuario(correo):
+            return False
+        
+        h = hashlib.new('sha256', bytes(contraseña,'utf-8'))
+        h = h.hexdigest()
+        query = "INSERT INTO usuario(correo, contraseña, nombre) VALUES (%s, %s, %s)"
+        
+        self.cursor.execute(query, (correo, h, nombre))
+        self.connection.commit()
+
+        return True
+
+    def existe_usuario(self, correo):
+        query = "SELECT COUNT(*) FROM usuario WHERE correo = %s"
+        self.cursor.execute(query, (correo,))
+
+        return self.cursor.fetchone()[0] == 1
+
+    def iniciar_sesion(self, correo, contraseña):
+        h = hashlib.new('sha256', bytes(contraseña, 'utf-8'))
+        h = h.hexdigest()
+
+        query = "SELECT id, nombre FROM usuario WHERE correo = %s AND contraseña = %s"
+        self.cursor.execute(query, (correo, h))
+
+        id = self.cursor.fetchone()
+
+        if id:
+            return id[0], id[1], True
+        else:
+            return None, None, False
--- main.py
+++ main.py
@@ -1,11 +1,49 @@
-from flask import Flask
+from flask import Flask, request
+from flask.json import jsonify
 from db.database import Database
 
 def run_server():
     app = Flask(__name__)
-    bd = Database()
+    db = Database()
 
+    @app.route('/api/v1/usuarios', methods = ['POST'])
+    def usuarios():
+        if request.method == 'POST' and request.is_json:
+            try:
+                data = request.get_json()
+                print(data)
+
+                correo = data['correo']
+                contraseña = data['contraseña']
+                nombre = data['nombre']
+
+                if(db.agregar_usuario(correo, contraseña, nombre)):
+                    return jsonify(code = 'ok')
+                else:
+                    return jsonify(code = 'existe')
+
+            except Exception as e:
+                print(e)
+                return jsonify(code = 'error')
+    @app.route('/api/v1/sesiones', methods = ['POST'])
+    def sesiones():
+        if request.method == 'POST' and request.is_json:
+            try:
+                data = request.get_json()
+                correo = data['correo']
+                contraseña = data['contraseña']
+
+                id, nombre, ok = db.iniciar_sesion(correo, contraseña)
+
+                if ok:
+                    return jsonify(code = 'ok', id = id, nombre = nombre)
+                else:
+                    return jsonify(code = 'no')
+            except Exception as e:
+                print(e)
+                return jsonify(code = 'error')
+            
     app.run(debug = True)
-    
+
 if __name__ == '__main__':
     run_server()
